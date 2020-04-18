e0b962c3508abf940097077305eb960e8990218c
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-04-18 11:48:36 +0200

    Aufgabe 3

---
+++ src/main/java/com/jorgeparavicini/OOPTest/Aufgabe3.java
@@ -0,0 +1,62 @@
+package com.jorgeparavicini.OOPTest;
+
+/* OUTPUT
+X
+Y
+X.m1
+Y.m1
+X
+Y
+Z
+X.m1
+Y.m1
+Z.m1
+ */
+public class Aufgabe3
+{
+    private class X {
+        X() {
+            System.out.println("X");
+        }
+
+        void m1() {
+            System.out.println("X.m1");
+        }
+    }
+
+    private class Y extends X {
+        Y() {
+            System.out.println("Y");
+        }
+
+        void m1() {
+            super.m1();
+            System.out.println("Y.m1");
+        }
+    }
+
+    private class Z extends Y {
+        Z() {
+            System.out.println("Z");
+        }
+
+        void m1() {
+            super.m1();
+            System.out.println("Z.m1");
+        }
+    }
+
+    private void test() {
+        X x = new Y();
+        x.m1();
+
+        Z z=new Z();
+        z.m1();
+    }
+
+    public static void main(String[] args)
+    {
+        Aufgabe3 instance = new Aufgabe3();
+        instance.test();
+    }
+}
